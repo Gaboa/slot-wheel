d781c0c1646074ceb740e5a67c16b0e407c7199c
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-12-07 20:25:31 +0200

    Example with screen usage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import './index.css'
 import { Application } from 'pixi.js'
 import { TweenMax } from 'gsap'
 
-import { Wheel } from './components'
-import { SpriteElement } from './components'
+import { Screen, Wheel, SpriteElement } from './components'
 
 const game = new Application({
     width:  1280,
@@ -14,59 +13,25 @@ game.loader.add({
     name: 'elements', url: 'img/elements.json'
 })
 game.loader.load(
-    () => {
-        const wheels = []
-        for (let i = 0; i < 3; i++) {
-            wheels.push(
-                new Wheel({
-                    container: game.stage,
-                    x: game.view.width  * 0.5 + (i - 1) * 256,
-                    y: game.view.height * 0.5,
-                
-                    dir: 'down',
-                
-                    el: {
-                        Element: SpriteElement,
-                        amount:  5,
-                        aside:   1,
-                        width:   256,
-                        height:  240
-                    },
-                
-                    // Amount param in anims config should be divided by el.amount
-                    start: {
-                        anims:  [
-                            { type: 'static', el: 'j' },
-                            { type: 'static', el: 'q' },
-                            { type: 'static', el: 'j' },
-                            { type: 'static', el: 'q' },
-                            { type: 'static', el: 'j' }
-                        ],
-                        amount: 10,
-                        time:   0.5,
-                        ease:   Back.easeIn.config(0.6)
-                    },
-                    loop: {
-                        anims:  [
-                            { type: 'blur', el: 'j' },
-                            { type: 'blur', el: 'q' },
-                            { type: 'blur', el: 'k' },
-                            { type: 'blur', el: 'a' }
-                        ],
-                        amount: 15,
-                        time:   0.12,
-                        ease:   Linear.easeNone
-                    },
-                    end: {
-                        amount: 10,
-                        time:   0.5,
-                        ease:   Back.easeOut.config(0.6)
-                    }
-                })
-            )
-        }
+    function() {
+
+        const screen = new Screen({
+            container: game.stage,
+            x: game.view.width  * 0.5,
+            y: game.view.height * 0.5,
+            config: {
+                amount: 3,
+                dir: 'right',
+            }
+        })
         
-        window.wheels = wheels
+        window.screen = screen
         window.game = game
     }
 )
+
+window.endData = [
+    [{ type: 'static', el: 'a' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' } ],
+    [{ type: 'static', el: 'a' }, { type: 'static', el: 'k' }, { type: 'static', el: 'q' }, { type: 'static', el: 'k' }, { type: 'static', el: 'j' } ],
+    [{ type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'k' }, { type: 'static', el: 'a' }, { type: 'static', el: 'j' } ]
+]
