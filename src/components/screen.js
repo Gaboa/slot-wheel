78307e4fc42020485902381a2e3e81e2c8a742cb
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-12-08 19:05:44 +0200

    Add basic screen logic

--- src/components/screen.js
+++ src/components/screen.js
@@ -4,6 +4,7 @@ import { Wheel } from "./wheel"
 import { SpriteElement } from "./element"
 import { TweenMax } from 'gsap'
 import { Observable } from 'rxjs'
+import { Subject } from 'rxjs/Subject';
 
 const defaultConfig = {
     Wheel,
@@ -17,19 +18,19 @@ const defaultConfig = {
         Element: SpriteElement,
         amount:  5,
         aside:   1,
-        width:   256,
-        height:  240
+        width:   256 * 0.5,
+        height:  240 * 0.5
     },
 
     start: {
         anims: [
             { type: 'static', el: 'j' },
-            { type: 'static', el: 'q' },
             { type: 'static', el: 'j' },
-            { type: 'static', el: 'q' },
+            { type: 'static', el: 'j' },
+            { type: 'static', el: 'j' },
             { type: 'static', el: 'j' }
         ],
-        amount: 10,
+        amount: 15,
         time:   0.5,
         ease:   Back.easeIn.config(0.6)
     },
@@ -42,14 +43,20 @@ const defaultConfig = {
             { type: 'blur', el: 'a' }
         ],
         amount: 5,
-        time:   0.12,
+        time:   0.12 * 0.5,
         ease:   Linear.easeNone
     },
 
     end: {
-        amount: 10,
+        amount: 15,
         time:   0.5,
         ease:   Back.easeOut.config(0.6)
+    },
+
+    roll: {
+        normal: 1,
+        fast: 2,
+        immediate: 5
     }
 
 }
@@ -84,10 +91,52 @@ class Screen extends Container {
     enableStreams() {
         this.subs = []
 
-        this.$ = Observable.merge(...this.wheels.map(w => w.$))
+        this.$ = new Subject()
+
+        this.wheels$ = Observable.merge(...this.wheels.map(w => w.$))
+        this.wheels$.subscribe(e => this.$.next(e))
+
+        this.subs.push(
+        this.logSub = this.$
+            .filter(e => e.type !== 'WHEEL_EL_SWITCH')
+            .subscribe(e => console.log(e)))
+
+        this.subs.push(
+        this.rollEndSub = this.$
+            .filter(e => e.type === 'WHEEL')
+            .filter(e => e.tween === 'END')
+            .filter(e => e.state === 'COMPLETE')
+            .bufferCount(this.config.amount)
+            .subscribe(e => this.$.next({ type: 'ROLL', state: 'END', time: performance.now() })))
+
+        this.subs.push(
+        this.maxLoopCountSub = this.$
+            .filter(e => e.type === 'WHEEL')
+            .filter(e => e.tween === 'LOOP')
+            .filter(e => e.state === 'START')
+            .pluck('count')
+            .distinct(null, this.$.filter(e => e.type === 'ROLL').filter(e => e.state === 'END')) // Чистит свой кеш во время окончания крутки, что поз
+            .subscribe(n => this.$.next({ type: 'ROLL', state: 'MAX_LOOP', count: n })))
+        
+        this.subs.push(
+        this.setWheelMaxLoopCountSub = this.$
+            .filter(e => e.type === 'ROLL')
+            .filter(e => e.state === 'MAX_LOOP')
+            .subscribe(e => this.wheels.forEach(wheel => wheel.minLoops = e.count)))
+
+        this.subs.push(
+        this.clearWheelMaxLoopCountSub = this.$
+            .filter(e => e.type === 'ROLL')
+            .filter(e => e.state === 'START')
+            .subscribe(e => this.wheels.forEach(wheel => wheel.minLoops = null)))
+
     }
     disableStreams() {
         this.subs.forEach(sub => sub.unsubscribe())
+    }
+    disableMaxLoopStreams() {
+        this.setWheelMaxLoopCountSub.unsubscribe()
+        this.clearWheelMaxLoopCountSub.unsubscribe()
     }
 
     // Direction
@@ -145,8 +194,19 @@ class Screen extends Container {
     // Roll methods
     roll() {
         this.tw = TweenMax.staggerTo(this.wheels, 0.1, { alpha: 1, onStart() { this.target.roll() } }, this.config.dt)
+        this.$.next({ type: 'ROLL', state: 'START', time: performance.now() })
     }
-    fast() {}
+    fast() {
+        this.roll()
+        this.wheels.forEach(wheel => wheel.fast())
+    }
+    immediate() {
+        this.roll()
+        this.wheels.forEach(wheel => wheel.immediate())
+    }
+    setRollSpeed(speed) {
+        this.wheels.forEach(wheel => wheel.speed = speed)
+    }
 
     // Screen data manipulation
     setStartScreen(data) {
