9dc11a1d6745a4d75bf770e3600f9a81ab341d6d
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-12-14 23:53:08 +0200

    Add global width and height

--- src/index.js
+++ src/index.js
@@ -8,24 +8,36 @@ const game = new Application({
     width:  1280,
     height: 720
 })
+window.GAME_WIDTH = 1280
+window.GAME_HEIGHT = 720
 document.body.appendChild(game.view)
-game.loader.add({
-    name: 'elements', url: 'img/elements.json'
-})
-game.loader.load(
-    function() {
+game.loader.baseUrl = 'src/'
+game.loader
+    .add({ url: 'img/elements.json' })
+    .load((loader, resources) =>  {
 
         const screen = new Screen({
             container: game.stage,
-            x: game.view.width  * 0.5,
-            y: game.view.height * 0.5,
+            x: 0.5,
+            y: 0.5,
             config: {
                 amount: 5,
-                dir: 'down',
+                dir: 'up',
 
                 loop: {
                     amount: 20,
-                    time: 0.5
+                    time:   0.5
+                },
+
+                roll: {
+                    normal: 0.3,
+                    fast: 1.5
+                },
+
+                log: {
+                    screen: false,
+                    wheel: false,
+                    el: false
                 }
             },
             dt: 0.075
@@ -37,14 +49,14 @@ game.loader.load(
 )
 
 window.endHorizontalData = [
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' } ],
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'k' }, { type: 'static', el: 'q' }, { type: 'static', el: 'k' }, { type: 'static', el: 'j' } ],
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'k' }, { type: 'static', el: 'a' }, { type: 'static', el: 'j' } ]
+    [{ type: 'static', el: '2' }, { type: 'static', el: '4' }, { type: 'static', el: '6' }, { type: 'static', el: '8' }, { type: 'static', el: '10' } ],
+    [{ type: 'static', el: '2' }, { type: 'static', el: '4' }, { type: 'static', el: '6' }, { type: 'static', el: '8' }, { type: 'static', el: '10' } ],
+    [{ type: 'static', el: '2' }, { type: 'static', el: '4' }, { type: 'static', el: '6' }, { type: 'static', el: '8' }, { type: 'static', el: '10' } ]
 ]
 window.endVerticalData = [
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' }, { type: 'static', el: 'j' }  ],
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'k' }, { type: 'static', el: 'k' }, { type: 'static', el: 'k' }, { type: 'static', el: 'k' }  ],
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }  ],
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }  ],
-    [{ type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }, { type: 'static', el: 'a' }  ]
+    [{ type: 'static', el: '1' }, { type: 'static', el: '2' }, { type: 'static', el: '3' }, { type: 'static', el: '4' }, { type: 'static', el: '5' }  ],
+    [{ type: 'static', el: '1' }, { type: 'static', el: '2' }, { type: 'static', el: '3' }, { type: 'static', el: '4' }, { type: 'static', el: '5' }  ],
+    [{ type: 'static', el: '1' }, { type: 'static', el: '2' }, { type: 'static', el: '3' }, { type: 'static', el: '4' }, { type: 'static', el: '5' }  ],
+    [{ type: 'static', el: '1' }, { type: 'static', el: '2' }, { type: 'static', el: '3' }, { type: 'static', el: '4' }, { type: 'static', el: '5' }  ],
+    [{ type: 'static', el: '1' }, { type: 'static', el: '2' }, { type: 'static', el: '3' }, { type: 'static', el: '4' }, { type: 'static', el: '5' }  ]
 ]
