fb0e02d206fa6694638e11778eb689a9c994fb7c
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-12-15 17:16:49 +0200

    Remove old Element class

--- src/components/element.js
+++ src/components/element.js
@@ -1,62 +1,5 @@
+import { Subject } from 'rxjs/Subject';
 import { Container, Sprite } from '../utils'
-import { Graphics, Text } from 'pixi.js'
-import { Subject } from 'rxjs'
-
-const colors = [0xff00ff, 0x00ffff, 0xffff00, 0x444444, 0x0000ff]
-
-class Element extends Container {
-    constructor({
-        container,
-        x = 0,
-        y = 0,
-
-        width,
-        height,
-        index,
-        anim
-    }) {
-        super({ container, x, y })
-
-        this.w = width
-        this.h = height
-        this.anim = anim
-        this.index = index
-        this.color = colors[index % 5]
-
-        this.pivot.set(this.w * 0.5, this.h * 0.5)
-
-        this.addBG()
-        this.addText()
-    }
-
-    addBG() {
-        this.g = new Graphics()
-        this.g.beginFill(this.color)
-        this.g.drawRect(0, 0, this.w, this.h)
-        this.g.endFill()
-        this.g.cacheAsBitmap = true
-        this.addChild(this.g)
-    }
-
-    addText() {
-        this.t = new Text(this.anim, { align: 'center', fill: '#555' })
-        this.t.x = this.w * 0.5
-        this.t.y = this.h * 0.5
-        this.t.anchor.set(0.5)
-        this.addChild(this.t)
-    }
-
-    changeValue(newValue) {
-        this.value  = newValue
-        this.t.text = newValue
-    }
-
-    play(value) {
-        this.changeValue(value)
-    }
-}
-
-
 
 class SpriteElement extends Sprite {
     constructor({
@@ -103,4 +46,4 @@ class SpriteElement extends Sprite {
     }
 }
 
-export { Element, SpriteElement }
+export { SpriteElement }
