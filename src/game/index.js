849e8a0cd56b6fe18532391a2b674b1d0ff00f84
Gaboa <dmitry.zhabko@gmail.com>
2017-12-19 20:03:25 +0200

    Base of Game class

--- src/game/index.js
+++ src/game/index.js
@@ -1,6 +1,56 @@
+import defaultsDeep from 'lodash.defaultsdeep'
+import { Application } from 'pixi.js'
+
+import { DeviceManager } from './device'
 export { StateManager } from './state'
 export { DataManager } from './data'
-export { DeviceManager } from './device'
 export { RequestManager } from './request'
 export { AudioManager } from './audio'
-export { ParsingManager } from './parser'
+export { ParserManager } from './parser'
+
+class Game extends Application {
+
+    constructor({
+        
+        id,
+        fps = 30,
+        width,
+        height,
+
+        request = {},
+        parser = {},
+        device = {},
+        audio = {},
+        state = {},
+        data = {}
+
+    }) {
+        const dev = new DeviceManager({ mode: 'none' })
+        super({
+            width:  width  || window.GAME_WIDTH,
+            height: height || window.GAME_HEIGHT,
+            autoStart: false
+        })
+        document.querySelector(id).appendChild(this.view)
+        this.setFPS(fps)
+        
+        this.device = new DeviceManager(defaultsDeep({ id, renderer: this.renderer }, device))
+    }
+
+    setFPS(fps) {
+        this.fps = fps
+        this.frameCount = this.frameCount || 0
+        this.animate(performance.now())
+    }
+
+    animate(time) {
+        if (++this.frameCount >= Math.ceil(60 / this.fps)) {
+            this.ticker.update(time)
+            this.frameCount = 0
+        }
+        requestAnimationFrame(this.animate.bind(this))
+    }
+
+}
+
+export { Game }
