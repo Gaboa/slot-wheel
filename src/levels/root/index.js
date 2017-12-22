c9098fd25220c91cbc0ac5a60e1612d6028f701a
Gaboa <dmitry.zhabko@gmail.com>
2017-12-22 19:50:34 +0200

    Add Footer and Balance controllers

--- src/levels/root/index.js
+++ src/levels/root/index.js
@@ -1,7 +1,10 @@
-import { Subject } from "rxjs"
-import { Container } from "../../utils/container"
-import { Screen } from '../../components'
+import { Subject, Observable } from "rxjs"
+import { Container, Sprite } from "../../utils"
+import { Screen, Footer } from '../../components'
 import { Darkness } from "../preload/helpers"
+
+// TODO: Add level and value to subs
+// TODO: Create balance bindings for different modes
 
 class Root extends Container {
 
@@ -10,7 +13,16 @@ class Root extends Container {
         config
     }) {
         super({ container: game.stage, x: 0.5, y: 0.5 })
+        
         this.game = game
+        this.data = game.data
+        this.state = game.state
+
+        this.bg = new Sprite({
+            container: this,
+            texture: 'preload_bg',
+            name: 'bg'
+        })
 
         this.screen = new Screen({
             container: this,
@@ -38,13 +50,19 @@ class Root extends Container {
         })
         this.screen.addMask()
 
+        this.footer = new Footer({
+            container: this
+        })
+
         this.darkness = new Darkness({
             container: this,
             autoHide: true
         })
 
+        this.balanceCtrl = new BalanceController(game)
+
         this.enableStreams()
-        this.balance()
+        this.enableBalanceStreams()
 
     }
 
@@ -56,83 +74,200 @@ class Root extends Container {
             .filter(e => e.from === 'SCREEN')
             .filter(e => e.state === 'START')
             .subscribe(e => this.game.request.sendRoll({
-                value: this.game.data.balance.value.current,
-                level: this.game.data.balance.level.current
+                value: this.data.balance.value.current,
+                level: this.data.balance.level.current
             }))
 
-        this.game.data.screen$
+        this.screen.$
+            .filter(e => e.from === 'SCREEN')
+            .filter(e => e.state === 'START')
+            .subscribe(e => this.balanceCtrl.start())
+
+        this.screen.$
+            .filter(e => e.from === 'SCREEN')
+            .filter(e => e.state === 'END')
+            .subscribe(e => this.balanceCtrl.end())
+
+        this.data.screen$
             .filter(e => Array.isArray(e))
             .take(1)
             .subscribe(s => this.screen.setStartScreen(s))
 
-        this.game.data.screen$
+        this.data.screen$
             .filter(e => Array.isArray(e))
             .skip(1)
             .subscribe(s => this.screen.setEndScreen(s))
+
+        // Footer
+        this.game.ticker.add(this.footer.time.update.bind(this.footer.time))
+
+        // Fullscreen button
+        this.footer.buttons.fullscreen.$
+            .filter(e => e.type === 'DOWN')
+            .subscribe(e => this.state.settings.isFullscreen = !this.state.settings.isFullscreen)
+
+        this.state.settings.isFullscreen$
+            .subscribe(e => {
+                this.footer.buttons.fullscreen.to(e)
+                e ? this.game.device.enterFullscreen() : this.game.device.cancelFullscreen()
+            })
+
+        // Sound button
+        this.footer.buttons.sound.$
+            .filter(e => e.type === 'DOWN')
+            .subscribe(e => this.state.settings.isSound = !this.state.settings.isSound)
+
+        this.state.settings.isSound$
+            .subscribe(e => this.footer.buttons.sound.to(e))
+
+        // Sound button
+        this.footer.buttons.fast.$
+            .filter(e => e.type === 'DOWN')
+            .subscribe(e => this.state.settings.isFast = !this.state.settings.isFast)
+
+        this.state.settings.isFast$
+            .subscribe(e => this.footer.buttons.fast.to(e))
+
+        // Footer Balance
+        this.data.balance.currency$
+            .subscribe(e => this.footer.balance.setCurrency(e))
+
+        // Root bindings
+        this.data.balance.cash.sum$
+            .subscribe(e => this.footer.balance.bottom.left.set(e))
+        this.data.balance.cash.bet$
+            .subscribe(e => this.footer.balance.bottom.center.set(e))
+        this.data.balance.cash.win$
+            .subscribe(e => this.footer.balance.bottom.right.set(e))
+
+        this.data.balance.coin.sum$
+            .subscribe(e => this.footer.balance.top.left.set(e))
+        this.data.balance.coin.bet$
+            .subscribe(e => this.footer.balance.top.right.set(e))
     }
 
-    balance() {
+    enableBalanceStreams() {
         // Value
-        this.game.data.balance.value.index$
-            .subscribe(i => this.game.data.balance.value.current = this.game.data.balance.value.arr[i])
+        this.data.balance.value.index$
+            .subscribe(i => this.data.balance.value.current = this.data.balance.value.arr[i])
 
         // Up and Bottom stoppers
-        this.game.data.balance.value.index$
-            .filter(i => i > this.game.data.balance.value.arr.length - 1)
-            .subscribe(i => this.game.data.balance.value.index = this.game.data.balance.value.arr.length - 1)
+        this.data.balance.value.index$
+            .filter(i => i > this.data.balance.value.arr.length - 1)
+            .subscribe(i => this.data.balance.value.index = this.data.balance.value.arr.length - 1)
 
-        this.game.data.balance.value.index$
+        this.data.balance.value.index$
             .filter(i => i < 0)
-            .subscribe(i => this.game.data.balance.value.index = 0)
+            .subscribe(i => this.data.balance.value.index = 0)
 
         // Min + Max logic
-        this.game.data.balance.value.index$
+        this.data.balance.value.index$
             .subscribe(i => {
-                if (i === this.game.data.balance.value.arr.length - 1) {
-                    this.game.data.balance.value.max = true
-                    this.game.data.balance.value.min = false
+                if (i === this.data.balance.value.arr.length - 1) {
+                    this.data.balance.value.max = true
+                    this.data.balance.value.min = false
                 } else if (i === 0) {
-                    this.game.data.balance.value.max = false
-                    this.game.data.balance.value.min = true
+                    this.data.balance.value.max = false
+                    this.data.balance.value.min = true
                 } else {
-                    this.game.data.balance.value.max = false
-                    this.game.data.balance.value.min = false
+                    this.data.balance.value.max = false
+                    this.data.balance.value.min = false
                 }
             })
 
         // Level
-        this.game.data.balance.level.index$
-            .subscribe(i => this.game.data.balance.level.current = this.game.data.balance.level.arr[i])
+        this.data.balance.level.index$
+            .subscribe(i => this.data.balance.level.current = this.data.balance.level.arr[i])
     
         // Up and Bottom stoppers
-        this.game.data.balance.level.index$
-            .filter(i => i > this.game.data.balance.level.arr.length - 1)
-            .subscribe(i => this.game.data.balance.level.index = this.game.data.balance.level.arr.length - 1)
+        this.data.balance.level.index$
+            .filter(i => i > this.data.balance.level.arr.length - 1)
+            .subscribe(i => this.data.balance.level.index = this.data.balance.level.arr.length - 1)
         
-        this.game.data.balance.level.index$
+        this.data.balance.level.index$
             .filter(i => i < 0)
-            .subscribe(i => this.game.data.balance.level.index = 0)
+            .subscribe(i => this.data.balance.level.index = 0)
 
         // Min + Max logic
-        this.game.data.balance.level.index$
+        this.data.balance.level.index$
             .subscribe(i => {
-                if (i === this.game.data.balance.level.arr.length - 1) {
-                    this.game.data.balance.level.max = true
-                    this.game.data.balance.level.min = false                    
+                if (i === this.data.balance.level.arr.length - 1) {
+                    this.data.balance.level.max = true
+                    this.data.balance.level.min = false                    
                 } else if (i === 0) {
-                    this.game.data.balance.level.max = false
-                    this.game.data.balance.level.min = true
+                    this.data.balance.level.max = false
+                    this.data.balance.level.min = true
                 } else {
-                    this.game.data.balance.level.max = false
-                    this.game.data.balance.level.min = false
+                    this.data.balance.level.max = false
+                    this.data.balance.level.min = false
                 }
             })
+
+        // Change level => change Coin Bet
+        this.data.balance.level.current$
+            .subscribe(level => this.data.balance.coin.bet = level * this.data.lines.length)
+
+        // Change value => change Coin Sum
+        this.data.balance.value.current$
+            .subscribe(value => this.data.balance.coin.sum = Math.floor(this.data.balance.cash.sum * 100 / value))
+            
+        // Change level or value => change Cash Bet
+        this.data.balance.value.current$
+            .combineLatest(this.data.balance.level.current$)
+            .subscribe(([value, level]) => this.data.balance.cash.bet = level * this.data.lines.length * value / 100)
 
     }
 
     disableStreams() {
         this.subs.forEach(sub => sub.unsubscribe())
         this.$.complete()
+    }
+
+}
+
+class BalanceController {
+
+    constructor(game) {
+        this.game = game
+        this.data = game.data
+        this.state = game.state
+        this.balance = this.data.balance
+    }
+
+    level({
+        up = false,
+        down = false,
+        max = false
+    }) {
+        if (up && !this.balance.level.max)
+            this.balance.level.index++
+        if (down && !this.balance.level.min)
+            this.balance.level.index--
+        if (max && !this.balance.level.max)
+            this.balance.level.index = this.balance.level.arr.length - 1
+    }
+
+    value({
+        up = false,
+        down = false,
+        max = false
+    }) {
+        if (up && !this.balance.value.max)
+            this.balance.value.index++
+        if (down && !this.balance.value.min)
+            this.balance.value.index--
+        if (max && !this.balance.value.max)
+            this.balance.value.index = this.balance.value.arr.length - 1
+    }
+
+    start() {
+        this.balance.coin.sum = this.balance.coin.sum - this.balance.coin.bet
+        this.balance.cash.sum = (this.balance.cash.sum * 100 - this.balance.cash.bet * 100) / 100
+        this.balance.cash.win = 0
+    }
+
+    end() {
+        
     }
 
 }
