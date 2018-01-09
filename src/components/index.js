40320198fac53bb169a8a676da27ad5b24a0730d
Gaboa <dmitry.zhabko@gmail.com>
2018-01-09 20:47:44 +0200

    Add MobileButtons to Components

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ export { Element, SpriteElement } from "./element"
 export { Wheel } from "./wheel"
 export { Screen } from "./screen"
 export { Footer } from "./footer"
-export { Machine } from "./machine"
+export { MobileButtons } from "./mobile"
+export { Machine, WinTable } from "./machine"
 export { Numbers, WinNumber } from './numbers'
 export { Lines, Line, Part } from './lines'
 export { Panel, Buttons, Balance, AutoCounter, AutoPanel } from './panel'
