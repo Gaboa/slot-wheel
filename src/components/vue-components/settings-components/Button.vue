ff7dd7ad402cb8843d4867bc6c636559a0c5c418
Olya <olga.akinshyna@gmail.com>
2018-01-10 15:01:56 +0200

    settings vue component

---
+++ src/components/vue-components/settings-components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class=setting__button>{{name}}</button>
+</template>
+<script>
+export default {
+    props:['name', 'isActive']
+}
+</script>
+<style scoped>
+    .setting__button{
+        width: 25%;
+        height: 3.5vw;
+        font-size: 1.5vw;
+        border: none;
+        color: #000000;
+        cursor: pointer;
+    }
+    .active{
+        background-color: #2f7eb7;
+        color: #ffffff;
+    }
+</style>
+
+
---
+++ src/components/vue-components/settings-components/RadioButton.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="settings__row">
+            <span class="settings__name">{{details[0].name}}</span>
+            <slot></slot>
+            <span class="settings__item">
+                <div class="checkbox">
+                    <input class="checkbox__input" type="checkbox" :name="details[0].id" :id="details[0].id" checked="">
+                    <label class="checkbox__label" :for="details[0].id"></label>
+                </div>
+            </span>
+    </div>
+</template>
+<script>
+export default {
+    props:['details']
+}
+</script>
+<style scoped>
+.checkbox {
+    position: relative;
+    width: 2.5vw;
+    height: 2.5vw;
+}
+.checkbox__input {
+    display: none;
+}
+.checkbox__label {
+    position: relative;
+    z-index: 5;
+    display: block;
+    box-sizing: border-box;
+    width: 2.5vw;
+    height: 2.5vw;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    background: transparent;
+    cursor: pointer;
+    transition: all .3s ease-in-out;
+    box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2), inset 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
+}
+.checkbox__input:checked~.checkbox__label {
+    border-width: 0.9vw;
+    background: #5FD17A;
+    box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2), inset 2px 2px 4px 0 rgba(0, 0, 0, 0);
+}
+
+</style>
+
