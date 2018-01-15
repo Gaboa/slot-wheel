06c900a9b93c826c039dbc546d28b5a4af24caa9
Gaboa <dmitry.zhabko@gmail.com>
2018-01-15 19:42:52 +0200

    Remove SpriteElement class

--- src/components/element.js
+++ src/components/element.js
@@ -81,7 +81,6 @@ class Element extends Container {
         this.$ = new Subject()
 
         this.spines.forEach(spine => this.subs.push(spine.$.subscribe(e => this.$.next(e))))
-
     }
 
     disable() {
@@ -187,49 +186,5 @@ class Element extends Container {
     }
 
 }
-class SpriteElement extends Sprite {
-    constructor({
-        container,
-        x = 0,
-        y = 0,
 
-        width,
-        height,
-        index,
-        anim
-    }) {
-        super({ container, x, y, texture: 'static_j' })
-        this.w = width
-        this.h = height
-
-        this.index = index
-        this.anchor.set(0.5)
-
-        if (anim)
-            this.play(this.anim)
-        
-        this.enableStreams()
-    }
-    enableStreams() {
-        this.$ = new Subject()
-        this.subs = []
-    }
-    disableStreams() {
-        this.subs.forEach(sub => sub.unsubscribe())
-    }
-    play(anim) {
-        this.anim = anim
-        this.texture = PIXI.utils.TextureCache[`${anim.type}_${anim.el}`]
-        this.$.next({ from: 'EL', state: 'play', anim, el: this })
-    }
-    show() {
-        this.visible = true
-        this.$.next({ from: 'EL', state: 'show', el: this })
-    }
-    hide() {
-        this.visible = false
-        this.$.next({ from: 'EL', state: 'hide', el: this })
-    }
-}
-
-export { SpriteElement, Element }
+export { Element }
