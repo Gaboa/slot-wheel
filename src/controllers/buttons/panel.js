6bac4318890e1e15b8ee9e445372615756082b80
Gaboa <dmitry.zhabko@gmail.com>
2018-01-16 16:59:45 +0200

    Refactor Panel controller

--- src/controllers/buttons/panel.js
+++ src/controllers/buttons/panel.js
@@ -6,19 +6,38 @@ const defaultConfig = {
         minus: true,
         plus: true,
         min: true,
-        max: true
+        max: true,
+        keyboard: true
     },
     value: {
         minus: true,
         plus: true,
         min: true,
-        max: true
+        max: true,
+        keyboard: true
     },
-    auto: true,
-    item: true,
-    spin: true,
-    anim: true,
-    stop: true,
+    auto: {
+        button: true,
+        delta: 1500
+    },
+    item: {
+        button: true,
+        autoplay: true,
+        delta: 1500
+    },
+    spin: {
+        button:   true,
+        keyboard: true
+    },
+    anim: {
+        start: true,
+        state: true,
+        complete: true
+    },
+    stop: {
+        button: true,
+        autoplay: true
+    },
     max: true
 }
 
@@ -45,132 +64,172 @@ class PanelButtonsController {
     enable() {
         this.subs = []
 
-        // Spin
-        if (this.config.spin)
+        // ------  Spin  ------
+        // Spin button starts Screen roll
+        if (this.config.spin.button)
         this.subs.push(
         this.spinSub = this.buttons.spin.down$
-            .merge(Observable.fromEvent(document, 'keyup')
+            .merge(!this.config.spin.keyboard 
+                ? Observable.empty()
+                : Observable.fromEvent(document, 'keyup')
                 .filter(e => this.buttons.spin.enabled)
                 .map(e => String(e.code).toUpperCase())
                 .filter(code => code === 'SPACE'))
             .subscribe(e => this.machine.screen.roll()))
-        
-        // Anim of center button
-        if (this.config.anim)
+
+
+        // ------  Anim  ------
+        // Anim button Start
+        if (this.config.anim.start)
         this.subs.push(
         this.animStartSub = this.buttons.anim.$
             .filter(e => e.type === 'START')
             .subscribe(e => this.buttons.changeButtonTo('anim')))
 
-        if (this.config.anim)
+        // Anim button Complete
+        if (this.config.anim.complete)
         this.subs.push(
         this.animCompleteSub = this.buttons.anim.$
             .filter(e => e.type === 'COMPLETE')
             .map(e => e.anim.split('_')[1])
             .subscribe(state => this.buttons.changeButtonTo(state)))
 
-        if (this.config.anim)
+        // Anim button State
+        if (this.config.anim.state)
         this.subs.push(
         this.animStateSub = this.state.button$
             .subscribe(e => this.buttons.changeTo(e)))
 
+
+        // ------  Auto  ------
         // Auto Button
-        if (this.config.auto)
+        if (this.config.auto.button)
         this.subs.push(
         this.autoSub = this.buttons.auto.down$
-            .throttleTime(1500)
-            .subscribe(e => this.state.button = (this.state.button === 'spin')
+            .throttleTime(this.config.auto.delta)
+            .subscribe(e => (this.state.button === 'spin')
                 ? this.state.button = 'auto'
                 : this.state.button = 'spin' ))
 
-        // AutoItem in AutoPanel
-        if (this.config.item)
+
+        // ------  AutoItem  ------
+        // AutoItem change button to Stop
+        if (this.config.item.button)
         this.subs.push(
-        this.autoItemSub = this.buttons.panel.$
-            .throttleTime(1500)
+        this.autoItemButtonSub = this.buttons.panel.$
+            .throttleTime(this.config.item.delta)
             .map(e => e.value)
-            .subscribe(value => {
-                this.state.button = 'stop'
-                this.state.autoplay = value
-            }))
+            .subscribe(value => this.state.button = 'stop'))
 
-        // Stop Button
-        if (this.config.stop)
+        // AutoItem starts autoplay
+        if (this.config.item.autoplay)
         this.subs.push(
-        this.stopSub = this.buttons.stop.down$
-            .subscribe(e => {
-                this.state.button = 'spin'
-                this.state.autoplay = null
-            }))
+        this.autoItemAutoplaySub = this.buttons.panel.$
+            .throttleTime(this.config.item.delta)
+            .map(e => e.value)
+            .subscribe(value => this.state.autoplay = value ))
 
-        // Max
+
+        // ------  Stop  ------
+        // Stop Button change button to Spin
+        if (this.config.stop.button)
+        this.subs.push(
+        this.stopButtonSub = this.buttons.stop.down$
+            .subscribe(e => this.state.button = 'spin' ))
+
+        // Stop Button stops Autoplay
+        if (this.config.stop.autoplay)
+        this.subs.push(
+        this.stopAutoplaySub = this.buttons.stop.down$
+            .subscribe(e => this.state.autoplay = null ))
+
+        // ------  Max  ------
+        // Max Button change Level to Maximum amount
         if (this.config.max)        
         this.subs.push(
         this.maxSub = this.buttons.max.down$
             .subscribe(e => this.balance.level.index = this.balance.level.arr.length - 1))
         
-        // Level - Plus / Minus
-        if (this.config.level.minus)        
+        // ------  Level  ------
+        // Level Minus
+        if (this.config.level.minus)
         this.subs.push(
         this.levelMinusSub = this.buttons.level.minus.down$
-            .merge(Observable.fromEvent(document, 'keyup')
+            .merge(!this.config.level.keyboard 
+                ? Observable.empty()
+                : Observable.fromEvent(document, 'keyup')
                 .filter(e => this.buttons.level.minus.enabled)
                 .map(e => String(e.code).toUpperCase())
                 .filter(code => code === 'ARROWDOWN'))
             .subscribe(e => this.balance.level.index--))
-        
-        if (this.config.level.plus)                
+            
+        // Level Plus
+        if (this.config.level.plus)
         this.subs.push(
         this.levelPlusSub = this.buttons.level.plus.down$
-            .merge(Observable.fromEvent(document, 'keyup')
+            .merge(!this.config.level.keyboard 
+                ? Observable.empty()
+                : Observable.fromEvent(document, 'keyup')
                 .filter(e => this.buttons.level.plus.enabled)
                 .map(e => String(e.code).toUpperCase())
                 .filter(code => code === 'ARROWUP'))
             .subscribe(e => this.balance.level.index++))
 
-        // Value - Plus / Minus
+        // Level Max - Plus button
+        if (this.config.level.max)
+        this.subs.push(
+        this.levelMaxSub = this.balance.level.max$
+            .subscribe(e => this.buttons.level.plus.max(e)))
+
+        // Level Max - Max button 
+        if (this.config.level.max)
+        this.subs.push(
+        this.levelMaxMaxSub = this.balance.level.max$
+            .subscribe(e => this.buttons.max.max(e)))
+        
+        // Level Min
+        if (this.config.level.min)        
+        this.subs.push(
+        this.levelMinSub = this.balance.level.min$
+            .subscribe(e => this.buttons.level.minus.min(e)))
+
+        // ------  Value  ------
+        // Value Minus
         if (this.config.value.minus)                
         this.subs.push(
         this.valueMinusSub = this.buttons.value.minus.down$
-            .merge(Observable.fromEvent(document, 'keyup')
+            .merge(!this.config.value.keyboard 
+                ? Observable.empty()
+                : Observable.fromEvent(document, 'keyup')
                 .filter(e => this.buttons.value.minus.enabled)
                 .map(e => String(e.code).toUpperCase())
                 .filter(code => code === 'ARROWLEFT'))
             .subscribe(e => this.balance.value.index--))
         
+        // Value Plus
         if (this.config.value.plus)                
         this.subs.push(
         this.valuePlusSub = this.buttons.value.plus.down$
-            .merge(Observable.fromEvent(document, 'keyup')
+            .merge(!this.config.value.keyboard
+                ? Observable.empty()
+                : Observable.fromEvent(document, 'keyup')
                 .filter(e => this.buttons.value.plus.enabled)
                 .map(e => String(e.code).toUpperCase())
                 .filter(code => code === 'ARROWRIGHT'))
             .subscribe(e => this.balance.value.index++))
         
-        // Min / Max stoppers for value buttons
+        // Value Max
         if (this.config.value.max)                        
         this.subs.push(
         this.valueMaxSub = this.balance.value.max$
             .subscribe(e => this.buttons.value.plus.max(e)))
         
+        // Value Min
         if (this.config.value.min)                                
         this.subs.push(
         this.valueMinSub = this.balance.value.min$
             .subscribe(e => this.buttons.value.minus.min(e)))
         
-        // Min / Max stoppers for level buttons
-        if (this.config.level.max)                                
-        this.subs.push(
-        this.levelMaxSub = this.balance.level.max$
-            .subscribe(e => {
-                this.buttons.level.plus.max(e)
-                this.buttons.max.max(e)
-            }))
-        
-        if (this.config.level.min)                                        
-        this.subs.push(
-        this.levelMinSub = this.balance.level.min$
-            .subscribe(e => this.buttons.level.minus.min(e)))
     }
 
     disable() {
