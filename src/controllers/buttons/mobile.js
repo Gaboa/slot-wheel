e7b8ac53f4591f57b05e6979c5fcc1731ed2268f
Gaboa <dmitry.zhabko@gmail.com>
2018-01-16 17:00:26 +0200

    Little refactor MobileButtons controller

--- src/controllers/buttons/mobile.js
+++ src/controllers/buttons/mobile.js
@@ -5,8 +5,10 @@ const defaultConfig = {
     auto: true,
     spin: true,
     bet: true,
-    sound: true,
-    state: true
+    sound: {
+        button: true,
+        state: true
+    }
 }
 
 class MobileButtonsController {
@@ -22,6 +24,7 @@ class MobileButtonsController {
         this.level = game.root
         this.data  = game.data
         this.state = game.state
+        this.settings = game.state.settings
         this.balance = this.data.balance
         this.footer  = this.level.footer
         this.machine = this.level.machine
@@ -34,45 +37,41 @@ class MobileButtonsController {
     enable() {
         this.subs = []
 
-        // Menu
+        // Menu Button
         if (this.config.menu)
         this.subs.push(
         this.menuSub = this.buttons.menu.down$
             .subscribe(e => this.menu.open('settings')))            
 
-        // Autoplay
+        // Autoplay Button
         if (this.config.auto)
         this.subs.push(
         this.autoSub = this.buttons.auto.down$
             .subscribe(e => this.menu.open('auto')))
 
-        // Spin
+        // Spin Button
         if (this.config.spin)
         this.subs.push(
         this.spinSub = this.buttons.spin.down$
             .subscribe(e => this.machine.screen.roll()))
 
-        // Autoplay
+        // Bet Button
         if (this.config.bet)
         this.subs.push(
         this.betSub = this.buttons.bet.down$
             .subscribe(e => this.menu.open('bet')))
 
-        // Sound
-        if (this.config.sound)
+        // Sound Button
+        if (this.config.sound.button)
         this.subs.push(
-        this.soundSub = this.buttons.sound.down$
-            .subscribe(e => this.state.settings.isSound = !this.state.settings.isSound))
+        this.soundButtonSub = this.buttons.sound.down$
+            .subscribe(e => this.settings.isSound = !this.settings.isSound))
 
-        if (this.config.sound)
+        // Sound State
+        if (this.config.sound.state)
         this.subs.push(
-        this.soundStateSub = this.state.settings.isSound$
+        this.soundStateSub = this.settings.isSound$
             .subscribe(e => this.buttons.sound.to(e)))
-
-        // if (this.config.state)
-        // this.subs.push(
-        // this.buttonStateSub = this.state.button$
-        //     .subscribe(e => this.buttons.changeTo(e)))
     }
 
     disable() {
