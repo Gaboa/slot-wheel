53a8fc444fb98c4d32ed134a744957953c1731e1
Gaboa <dmitry.zhabko@gmail.com>
2018-01-16 17:00:05 +0200

    Refactor MobileMenu controller

--- src/controllers/buttons/menu.js
+++ src/controllers/buttons/menu.js
@@ -2,29 +2,49 @@ import defaultsDeep from 'lodash.defaultsdeep'
 import { Observable } from 'rxjs/Observable'
 
 const defaultConfig = {
-    autoplay: true,
     close: true,
-    side: true,
+    item: {
+        button: true,
+        autoplay: true
+    },
     bet: {
         level: {
             plus: true,
             minus: true,
-            minMax: true,
-            max: true
+            min: true,
+            max: true,
+            maxBet: true
         },
         value: {
             plus: true,
             minus: true,
-            minMax: true
+            min: true,
+            max: true
         },
     },
     settings: {
-        effects: true,
-        music: true,
-        fast: true,
-        hand: true,
+        effects: {
+            button: true,
+            state: true
+        },
+        music: {
+            button: true,
+            state: true
+        },
+        fast: {
+            button: true,
+            state: true
+        },
+        hand: {
+            button: true,
+            state: true,
+            menu: true
+        },
+        quality: {
+            button: true,
+            state: true
+        },
         info: true,
-        quality: true,
     }
 }
 
@@ -56,22 +76,6 @@ class MobileMenuController {
     enable() {
         this.subs = []
 
-        // Autoplay menu
-        // Auto Buttons
-        if (this.config.autoplay)
-        this.subs.push(
-        this.autoItemSub = this.auto.$
-            .subscribe(value => {
-                this.menu.close()
-                this.state.autoplay = value
-            }))
-
-        // Side binding
-        if (this.config.side)
-        this.subs.push(
-        this.sideSub = this.state.settings.isRightSide$
-            .subscribe(value => this.menu.changeSide(value)))
-
         // Close button
         if (this.config.close)
         this.subs.push(
@@ -82,18 +86,29 @@ class MobileMenuController {
             this.menu.darkness.down$
         ).subscribe(value => this.menu.close()))
 
-        // Bet menu
+        // ------  Autoplay  ------
+
+        // ------  AutoItem  ------ 
+        // AutoItem -> Close menu
+        if (this.config.item.button)
+        this.subs.push(
+        this.autoItemButtonSub = this.auto.$
+            .subscribe(value => this.menu.close()))
+
+        // AutoItem -> Start Autoplay
+        if (this.config.item.autoplay)
+        this.subs.push(
+        this.autoItemAutoplaySub = this.auto.$
+            .subscribe(value => this.state.autoplay = value ))
+
+        // ------  Set Bet  ------
+
+        // ------  Level  ------
         // Level Bindings
         if (this.config.bet.level)
         this.subs.push(
         this.levelSub = this.balance.level.current$
             .subscribe(e => this.bet.level.text.set(e)))                
-
-        // Value Binding
-        if (this.config.bet.value)
-        this.subs.push(
-        this.valueSub = this.balance.value.current$
-            .subscribe(e => this.bet.value.text.set(e / 100)))                
 
         // Level Plus
         if (this.config.bet.level.plus)
@@ -108,10 +123,29 @@ class MobileMenuController {
             .subscribe(e => this.balance.level.index--))
 
         // Level Max
-        if (this.config.bet.level.max)
+        if (this.config.bet.level.maxBet)
         this.subs.push(
-        this.levelMaxSub = this.bet.max.down$
+        this.levelMaxBetSub = this.bet.max.down$
             .subscribe(e => this.balance.level.index = this.balance.level.arr.length - 1))
+
+        // Level Min
+        if (this.config.bet.level.max)                        
+        this.subs.push(
+        this.levelMaxSub = this.balance.level.max$
+            .subscribe(e => this.bet.level.plus.max(e)))
+        
+        // Level Max
+        if (this.config.bet.level.min)                                
+        this.subs.push(
+        this.levelMinSub = this.balance.level.min$
+            .subscribe(e => this.bet.level.minus.min(e)))
+
+        // ------  Value  ------
+        // Value Binding
+        if (this.config.bet.value)
+        this.subs.push(
+        this.valueSub = this.balance.value.current$
+            .subscribe(e => this.bet.value.text.set(e / 100)))                
 
         // Value Plus
         if (this.config.bet.value.plus)
@@ -125,96 +159,87 @@ class MobileMenuController {
         this.valueMinusSub = this.bet.value.minus.down$
             .subscribe(e => this.balance.value.index--))
 
-        // Level Min
-        if (this.config.bet.level.minMax)                        
-        this.subs.push(
-        this.levelMaxSub = this.balance.level.max$
-            .subscribe(e => this.bet.level.plus.max(e)))
-        
-        // Level Max
-        if (this.config.bet.level.minMax)                                
-        this.subs.push(
-        this.levelMinSub = this.balance.level.min$
-            .subscribe(e => this.bet.level.minus.min(e)))
-
         // Value Min
-        if (this.config.bet.value.minMax)                        
+        if (this.config.bet.value.max)                        
         this.subs.push(
         this.valueMaxSub = this.balance.value.max$
             .subscribe(e => this.bet.value.plus.max(e)))
         
         // Value Max
-        if (this.config.bet.value.minMax)                                
+        if (this.config.bet.value.min)                                
         this.subs.push(
         this.valueMinSub = this.balance.value.min$
             .subscribe(e => this.bet.value.minus.min(e)))
 
-        // Settings menu
-        // Settings effects
-        if (this.config.settings.effects)
+        // ------  Settings  ------
+        
+        // ------  Effects  ------
+        if (this.config.settings.effects.button)
         this.subs.push(
-        this.effectsSub = this.settings.effects.$
+        this.effectsButtonSub = this.settings.effects.$
             .subscribe(e => this.state.settings.isEffects = !this.state.settings.isEffects))
 
-        if (this.config.settings.effects)
+        if (this.config.settings.effects.state)
         this.subs.push(
         this.effectsStateSub = this.state.settings.isEffects$
             .subscribe(e => this.settings.effects.to(e)))  
 
-        // Settings music
-        if (this.config.settings.music)
+        // ------  Music  ------
+        if (this.config.settings.music.button)
         this.subs.push(
-        this.musicSub = this.settings.music.$
+        this.musicButtonSub = this.settings.music.$
             .subscribe(e => this.state.settings.isMusic = !this.state.settings.isMusic))
 
-        if (this.config.settings.music)
+        if (this.config.settings.music.state)
         this.subs.push(
         this.musicStateSub = this.state.settings.isMusic$
             .subscribe(e => this.settings.music.to(e)))  
 
-        // Settings fast spin
-        if (this.config.settings.fast)
+        // ------  Fast  ------
+        if (this.config.settings.fast.button)
         this.subs.push(
-        this.fastSub = this.settings.fast.$
+        this.fastButtonSub = this.settings.fast.$
             .subscribe(e => this.state.settings.isFast = !this.state.settings.isFast))
         
-        if (this.config.settings.fast)
+        if (this.config.settings.fast.state)
         this.subs.push(
         this.fastStateSub = this.state.settings.isFast$
-            .subscribe(e => {
-                this.settings.fast.to(e)
-                // ??? Where must be this part of logic
-                this.screen.setRollSpeed(this.screen.config.roll[e ? 'fast' : 'normal'])
-            }))                        
+            .subscribe(e => this.settings.fast.to(e)))
 
-        // Settings hand mode
-        if (this.config.settings.hand)
+        // ------  Hand  ------
+        if (this.config.settings.hand.button)
         this.subs.push(
-        this.handSub = this.settings.hand.$
+        this.handButtonSub = this.settings.hand.$
             .subscribe(e => this.state.settings.isRightSide = !this.state.settings.isRightSide))
 
-        if (this.config.settings.hand)
+        if (this.config.settings.hand.state)
         this.subs.push(
         this.handStateSub = this.state.settings.isRightSide$
             .subscribe(e => this.settings.hand.to(e)))  
+        
+        // TODO: ??? Change place for this logic
+        if (this.config.settings.hand.menu)
+        this.subs.push(
+        this.handMenuSub = this.state.settings.isRightSide$
+            .subscribe(value => this.menu.changeSide(value)))
 
-        // Settings hand mode
+        // ------  Info  ------
+        // TODO: Add Info handler
         if (this.config.settings.info)
         this.subs.push(
         this.handSub = this.settings.hand.$
             .subscribe(e => e))
 
-        // Settings fast spin
-        if (this.config.settings.quality)
+        // ------  Quality  ------
+        if (this.config.settings.quality.button)
         this.subs.push(
-        this.qualitySub = this.settings.quality.$
+        this.qualityButtonSub = this.settings.quality.$
             .subscribe(e => this.state.settings.isLowQuality = !this.state.settings.isLowQuality))
 
-        if (this.config.settings.quality)
+        if (this.config.settings.quality.state)
         this.subs.push(
         this.qualityStateSub = this.state.settings.isLowQuality$
             .subscribe(e => this.settings.quality.to(e)))  
-            
     
     }
 
