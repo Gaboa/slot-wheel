fd5de433f9273e0bd2b67b3621d7ad0e66dc108f
Gaboa <dmitry.zhabko@gmail.com>
2018-01-16 16:58:27 +0200

    Refactor DesktopBalance controller

--- src/controllers/balance/desktop.js
+++ src/controllers/balance/desktop.js
@@ -1,15 +1,11 @@
 import defaultsDeep from 'lodash.defaultsdeep'
-import { FooterBalanceController } from './footer'
 
 const defaultConfig = {
-    currency: true,
-    cash: {
-        sum: true,
-        bet: true,
-        win: true
-    },
     coin: {
-        sum: true,
+        sum: {
+            idle: true,
+            end:  true
+        },
         bet: true
     },
     level: true,
@@ -17,70 +13,72 @@ const defaultConfig = {
     lines: true
 }
 
-class DesktopBalanceController extends FooterBalanceController {
+class DesktopBalanceController {
 
     constructor({
         game,
         config,
         autoEnable = true
     }) {
-        super({ game, autoEnable, config: {
-            coin: {
-                sum: false,
-                bet: false
-            }
-        }})
- 
         this.config = defaultsDeep(config, defaultConfig)
 
-        this.machine    = this.level.machine
-        this.machineBal = this.machine.panel.balance
+        this.game  = game
+        this.level = game.root
+        this.data  = game.data
+        this.state = game.state
+        this.balance = this.data.balance
+
+        this.machine = this.level.machine
+        this.panel   = this.machine.panel.balance
 
         if (autoEnable) this.enable()
     }
 
     enable() {
-        super.enable()
-        
-        // Panel Balance
+        this.subs = []
+
         // Lines
         if (this.config.lines)
         this.subs.push(
         this.linesSub = this.data.lines$
-            .subscribe(e => this.machineBal.lines.set(e.length)))
+            .subscribe(e => this.panel.lines.set(e.length)))
         
         // Level
         if (this.config.level)
         this.subs.push(
         this.levelSub = this.balance.level.current$
-            .subscribe(e => this.machineBal.level.set(e)))                
+            .subscribe(e => this.panel.level.set(e)))                
 
         // Value
         if (this.config.value)
         this.subs.push(
         this.valueSub = this.balance.value.current$
-            .subscribe(e => this.machineBal.value.set(e / 100)))
+            .subscribe(e => this.panel.value.set(e / 100)))
         
         // Coin Bet
         if (this.config.coin.bet)
         this.subs.push(
         this.coinBetSub = this.balance.coin.bet$
-            .subscribe(e => this.machineBal.bet.set(e)))
+            .subscribe(e => this.panel.bet.set(e)))
         
-        // Coin Sum
-        if (this.config.coin.sum)
+        // Coin Sum in Idle
+        if (this.config.coin.sum.idle)
         this.subs.push(
-        this.coinSumStartSub = this.balance.coin.sum$
+        this.coinSumIdleSub = this.balance.coin.sum$
             .filter(e => !this.state.isRolling)
-            .subscribe(e => this.machineBal.sum.set(e)))
+            .subscribe(e => this.panel.sum.set(e)))
         
-        // Coin Sum at the Roll End
-        if (this.config.coin.sum)
+        // Coin Sum at Roll End
+        if (this.config.coin.sum.end)
         this.subs.push(
         this.coinSumEndSub = this.balance.coin.sum$
             .filter(e => this.state.isRolling)
             .sample(this.state.isRolling$)
-            .subscribe(e => this.machineBal.sum.set(e)))
+            .subscribe(e => this.panel.sum.set(e)))
+    }
+
+    disable() {
+        this.subs.forEach(s => s.unsubscribe())
     }
 
 }
