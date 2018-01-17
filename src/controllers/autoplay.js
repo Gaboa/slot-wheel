0943b20728d1ac37d101bbc55cf5379b60ae03be
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2018-01-17 17:57:33 +0200

    Big refactor of Autoplay Controller

--- src/controllers/autoplay.js
+++ src/controllers/autoplay.js
@@ -2,11 +2,29 @@ import defaultsDeep from 'lodash.defaultsdeep'
 import { Observable } from 'rxjs/Observable'
 
 const defaultConfig = {
-    start: true,
-    timer: true,
-    count: true,
-    end: true,
-    noRoot: true
+    start: {
+        counter: true,
+        button: true,
+        idle: true,
+        roll: true,
+        delay: 500,
+        save: true
+    },
+    count: {
+        roll: true,
+        change: true,
+        end: true
+    },
+    tick: {
+        win: 2000,
+        fail: 200
+    },
+    end: {
+        counter: true,
+        button: true,
+        idle: true,
+        next: true
+    }
 }
 
 class AutoplayController {
@@ -32,85 +50,135 @@ class AutoplayController {
         if (autoEnable) this.enable()
     }
 
-    enable() {
-        this.subs = []
-        
-        // Change Autoplay Counter
-        if (this.config.count)
+    enableStart() {
+        // -----  Start  -----
+        // When isAutoplay state = true -> visible counter
+        if (this.config.start.counter)
         this.subs.push(
-        this.autoCounterSub = this.state.autoplay$
-            .filter(e => e) // When autoplay is positive number
-            .subscribe(e => this.buttons.count.set(e))) // Change counter to new value
+        this.autoplayStartCounterSub = this.state.isAutoplay$
+            .filter(e => e)
+            .subscribe(e => this.buttons.count.visible = true ))
 
-        // Autoplay Start triggers when state.autoplay changes from null to positive number
-        if (this.config.start)
+        // When isAutoplay state = true -> change button to stop
+        if (this.config.start.button)
         this.subs.push(
-        this.autoStartSub = this.state.autoplay$
-            .distinctUntilChanged((prev, curr) => !(prev === null && curr > 0 || curr === null))
-            .filter(e => e !== null) // Last autoplay value must be not a null 
-            .subscribe(e => {
-                this.state.button = 'stop' // Change main button to stop
-                this.buttons.count.visible = true // Counter is visible now 
-                this.buttons.disableAll() // All buttons ( without Stop ) are disabled
-                this.data.auto.start = this.balance.cash.sum // Save start sum to check stop triggers on edges
+        this.autoplayStartButtonSub = this.state.isAutoplay$
+            .filter(e => e)
+            .subscribe(e => this.state.button = 'stop' ))
 
-                this.machine.screen.roll() // First Autoplay roll
-                this.state.autoplay-- // Decrease autoplay value
-            }))
-        
-        // Autoplay Timer
-        if (this.config.timer)
+        // When isAutoplay state = true -> save start sum
+        if (this.config.start.save)
         this.subs.push(
-        this.autoTimerSub = this.state.autoplay$
+        this.autoplayStartSaveSub = this.state.isAutoplay$
+            .filter(e => e)
+            .subscribe(e => this.data.autoplay.start = this.balance.cash.sum ))
+
+        // When isAutoplay state = true -> change idle state to false
+        if (this.config.start.idle)
+        this.subs.push(
+        this.autoplayStartIdleSub = this.state.isAutoplay$
+            .filter(e => e)
+            .subscribe(e => this.state.isIdle = false ))
+
+        // When isAutoplay state = true -> start roll after some delay
+        if (this.config.start.roll)
+        this.subs.push(
+        this.autoplayStartRollSub = this.state.isAutoplay$
+            .filter(e => e)
+            .delay(this.config.start.delay || 300)
+            .subscribe(e => this.machine.screen.roll() ))
+    }
+
+    enableCounter() {
+        // -----  Counter  -----
+        // On roll =>  decrease counter
+        if (this.config.count.roll)
+        this.subs.push(
+        this.autoplayCountRollSub = this.machine.screen.$
+            .filter(e => e.from  === 'SCREEN')
+            .filter(e => e.state === 'START')
+            .filter(e => this.state.isAutoplay)
+            .subscribe(e => this.data.autoplay.count--))
+        
+        // Change Counter Value
+        if (this.config.count.change)
+        this.subs.push(
+        this.autoplayCountChangeSub = this.data.autoplay.count$
+            .filter(e => Number(e) >= 0) 
+            .subscribe(e => this.buttons.count.set(e)))
+
+        // When Counter is not a positive number -> stop Autoplay
+        if (this.config.count.end)
+        this.subs.push(
+        this.autoplayCountEndSub = this.data.autoplay.count$
+            .filter(e => !(Number(e) > 0)) 
+            .subscribe(e => this.state.isAutoplay = false))
+    }
+
+    enableTicker() {
+        // -----  Ticker  -----
+        if (this.config.tick)
+        this.subs.push(
+        this.autoTickSub = this.data.autoplay.count$
             .sample(this.state.isRolling$.filter(e => !e)) // At the end of roll
             .switchMap(e => this.data.win.lines.length // If we have win lines
-                        ? Observable.of(e).delay(2000) // Delay 2 seconds
-                        : Observable.of(e).delay(200)) // If no win lines - delay 200ms
-            .filter(e => this.state.autoplay) // If autoplay is enabled now
+                ? Observable.of(e).delay(this.config.tick.win) // Delay 2 seconds
+                : Observable.of(e).delay(this.config.tick.fail)) // If no win lines - delay 200ms
+            .filter(e => this.state.isAutoplay) // If autoplay is enabled now
             .subscribe(e => {
-                if (this.checkStoppers()) this.state.autoplay = null // If we hanve stoppers and they triggered => stop autoplay
-                else {
-                    this.machine.screen.roll() // Roll the screen
-                    this.state.autoplay-- // And decrease autoplay
-                }
+                if (this.checkStoppers()) this.state.isAutoplay = false // If we hanve stoppers and they triggered => stop autoplay
+                else this.machine.screen.roll() // Roll the screen
             }))
-
-        // Autoplay End
-        if (this.config.end)
-        this.subs.push(
-        this.autoEndSub = this.state.autoplay$
-            .filter(e => !e) // When autoplay === 0 or null ( end or stop )
-            .subscribe(e => {
-                this.state.button = 'spin' // Return main button to default state
-                this.buttons.count.visible = false // Remove counter
-                this.buttons.count.set(0) // And set counter to 0
-                if (this.state.autoplay !== null) // When Autoplay is noyt null ( like 0 )
-                    this.state.autoplay = null // Set to null to know that autoplay ended
-            }))
-
-        // Stoping auto when we not rolling
-        if (this.config.end)
-        this.subs.push(
-        this.autoNoRootStopSub = this.state.autoplay$
-            .filter(e => !e) // When autoplay is stopped
-            .filter(e => !this.state.isRolling) // And we are not rolling now
-            .subscribe(e => this.buttons.enableAll())) // Enable buttons
-
-        // Stop auto when next is not 'root'
-        if (this.config.noRoot)
-        this.subs.push(
-        this.autoNoRootStopSub = this.state.next$
-            .filter(e => e !== 'root') // When NextMode is not Root
-            .sample(this.state.isRolling$.filter(e => !e)) // At the End of Roll
-            .subscribe(e => this.state.autoplay = null)) // Stop autoplay
 
     }
 
+    enableEnd() {
+        // -----  End  -----
+        // When isAutoplay state = false -> Hide counter
+        if (this.config.end.counter)
+        this.subs.push(
+        this.autoAutoplayEndCounterSub = this.state.isAutoplay$
+            .filter(e => !e)
+            .subscribe(e => this.buttons.count.visible = false))
+
+        // When isAutoplay state = false -> Button to Spin
+        if (this.config.end.button)
+        this.subs.push(
+        this.autoAutoplayEndButtonSub = this.state.isAutoplay$
+            .filter(e => !e)
+            .subscribe(e => this.state.button = 'spin'))
+
+        // When isAutoplay state = false -> Idle state to true
+        if (this.config.end.idle)
+        this.subs.push(
+        this.autoAutoplayEndIdleSub = this.state.isAutoplay$
+            .filter(e => !e)
+            .filter(e => !this.state.isRolling)
+            .subscribe(e => this.state.isIdle = true ))
+
+        // When NextMode is not Root -> stop Autoplay
+        if (this.config.end.next)
+        this.subs.push(
+        this.autoAutoplayEndNextSub = this.state.next$
+            .filter(e => e !== 'root')
+            .sample(this.state.isRolling$.filter(e => !e))
+            .subscribe(e => this.state.isAutoplay = false ))
+    }
+
+    enable() {
+        this.subs = []
+
+        this.enableStart()
+        this.enableCounter()
+        this.enableTicker()
+        this.enableEnd()
+    }
+
     checkStoppers() {
-        if (this.data.auto.increase
-        && (this.balance.cash.sum - this.data.auto.start >= this.data.auto.increase)) return true
-        if (this.data.auto.decrease
-        && (this.data.auto.start - this.balance.cash.sum >= this.data.auto.decrease)) return true
+        if (this.data.autoplay.increase
+        && (this.balance.cash.sum - this.data.autoplay.start >= this.data.autoplay.increase)) return true
+        if (this.data.autoplay.decrease
+        && (this.data.autoplay.start - this.balance.cash.sum >= this.data.autoplay.decrease)) return true
         return false
     }
 
