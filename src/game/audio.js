62bf0172e978d43a7272a4e824494ed20475c154
Nykolaj <knikolaik@gmail.com>
2018-01-22 13:53:15 +0200

    Refactor audio

--- src/game/audio.js
+++ src/game/audio.js
@@ -13,76 +13,80 @@ class AudioManager {
         src,
         sprites
     }){
-        this.sprites = sprites;
-        this._volume = 100;
+        this.sprites = sprites
+        this._volume = 1
 
         this.Howler = new Howl({
             src: src,
             mute: true,
             sprite: this.parseSprites()
-        });
+        })
 
-        this.$ = new Subject();
+        this.$ = new Subject()
 
+        this.addLoadEvent()
+    }
+
+    addLoadEvent() {
         this.Howler.once('load', () => {
-            this.createGroups();
+            this.createGroups()
 
-            setTimeout(() => {this.Howler.mute(false)
-                this.play('init')
-                this.volume = 0.1
+            setTimeout(() => {
+                this.Howler.mute(false)
+                this.$.next({ type: 'LOAD', state: 'COMPLETE' })
             }, 0)
         })
     }
 
     createGroups() {
-        this.music = this.createGroup('music', true);
-        this.effects = this.createGroup('effects', false);
+        this.music = this.createGroup('music', true)
+        this.effects = this.createGroup('effects', false)
     }
 
     createGroup(type, isStopPrevious) {
-        let typeSprites = this.sprites.filter(sound => sound.type === type);
+        let typeSprites = this.sprites.filter(sound => sound.type === type)
 
         return new SoundGroup({
             howler: this.Howler,
             sprites: typeSprites,
             isStopPrevious: isStopPrevious,
             stream: this.$
-        });
+        })
     }
 
     parseSprites() {
-        let parsed = {};
+        let parsed = {}
 
         this.sprites.forEach(sprite => {
-            let start = this.parseTime(sprite.start);
-            let end = this.parseTime(sprite.end);
-            let duration = end - start - 10;
-            let loop = sprite.loop;
-            let name = sprite.name;
+            let start = this.parseTime(sprite.start)
+            let end = this.parseTime(sprite.end)
+            let duration = end - start - 10
+            let loop = sprite.loop
+            let name = sprite.name
 
-            parsed[name] = [start, duration, loop];
-        });
+            parsed[name] = [start, duration, loop]
+        })
 
-        return parsed;
+        return parsed
     }
 
     parseTime(value) {
-        let splited = value.split('.');
-        let minutes = parseInt(splited[0] * 60 * 1000);
-        let seconds = parseInt(splited[1] * 1000);
-        let ms = parseInt(splited[2]);
+        let splited = value.split('.')
+        let minutes = parseInt(splited[0] * 60 * 1000)
+        let seconds = parseInt(splited[1] * 1000)
+        let ms = parseInt(splited[2])
 
-        return minutes + seconds + ms;
+        return minutes + seconds + ms
     }
 
     set volume(value) {
-        this.music.volume = value;
-        this.effects.volume = value;
-        this._volume = value;
+        this.music.volume = value
+        this.effects.volume = value
+        this._volume = value
     }
 
     get volume() {
-        return this._volume;
+        return this._volume
     }
 
     muteMusic() {
@@ -98,7 +102,7 @@ class AudioManager {
     }
 
     unmuteEffects() {
-        this.effects.unmute();
+        this.effects.unmute()
     }
 
     play(value) {
@@ -132,29 +136,29 @@ class AudioManager {
     }
 
     muteAll() {
-        this.muteMusic();
-        this.muteMusic();
+        this.muteMusic()
+        this.muteMusic()
     }
 
     unmuteAll() {
-        this.unmuteMusic();
-        this.unmuteMusic();
+        this.unmuteMusic()
+        this.unmuteMusic()
     }
 }
 
 class SoundGroup {
     constructor({howler, sprites, isStopPrevious = false, stream}) {
-        this.howler = howler;
-        this.sprites = sprites;
-        this.sounds = [];
-        this.isStopPrevious = isStopPrevious;
-        this.$ = stream;
+        this.howler = howler
+        this.sprites = sprites
+        this.sounds = []
+        this.isStopPrevious = isStopPrevious
+        this.$ = stream
 
-        this.createSoundsInstance();
+        this.createSoundsInstance()
     }
 
     createSoundsInstance() {
-        let howler = this.howler;
+        let howler = this.howler
         this.sprites.forEach(sprite => {
             let sound = new SoundInstance({
                 howler: this.howler,
@@ -162,48 +166,48 @@ class SoundGroup {
                 volume: sprite.volume,
                 loop: sprite.loop,
                 stream: this.$
-            });
+            })
 
-            this[sprite.name] = sound;
-            this.sounds.push(sound);
-        });
+            this[sprite.name] = sound
+            this.sounds.push(sound)
+        })
     }
 
     play(value) {
         if(this.isStopPrevious) {
-            this.stopPrevious(value);
+            this.stopPrevious(value)
         }
 
-        this[value].play();
+        this[value].play()
     }
 
     stopPrevious(value) {
-        this.currentMusic && this.currentMusic.stop();
-        this.currentMusic = this[value];
+        this.currentMusic && this.currentMusic.stop()
+        this.currentMusic = this[value]
     }
 
     stop(value) {
-        this[value].stop();
+        this[value].stop()
     }
 
     mute() {
-        this.sounds.forEach(sound => sound.mute());
+        this.sounds.forEach(sound => sound.mute())
     }
 
     unmute() {
-        this.sounds.forEach(sound => sound.unmute());
+        this.sounds.forEach(sound => sound.unmute())
     }
 
     fade({name, from, to, duration}) {
-        this[name].fade(from, to, duration);
+        this[name].fade(from, to, duration)
     }
 
     has(value) {
-        return this[value] !== undefined;
+        return this[value] !== undefined
     }
 
     set volume(value) {
-        this.sounds.forEach(sound => sound.volume = value);
+        this.sounds.forEach(sound => sound.volume = value)
     }
 }
 
@@ -214,57 +218,57 @@ class SoundInstance {
         volume = 1,
         stream
     }) {
-        this.howler = howler;
-        this.maxVolume = volume;
-        this.id = howler.play(name);
-        this.$ = stream;
-        this.name = name;
+        this.howler = howler
+        this.maxVolume = volume
+        this.id = howler.play(name)
+        this.$ = stream
+        this.name = name
 
-        this.volume = 1;
-        setTimeout(() => {this.stop()}, 0);
+        this.volume = 1
+        setTimeout(() => {this.stop()}, 0)
 
         this.howler.on('fade', (id => {
             if(id === this.id) {
-                this.$.next({type: 'fadeEnd', name: this.name, source: this});
+                this.$.next({type: 'FADE_END', name: this.name, source: this})
             }
         }))
     }
 
     play() {
-        this.howler.play(this.id);
+        this.howler.play(this.id)
 
-        this.$.next({type: 'start', name: this.name, source: this, volume: this.volume});
+        this.$.next({type: 'START', name: this.name, source: this, volume: this.volume})
     }
 
     stop() {
-        this.howler.stop(this.id);
+        this.howler.stop(this.id)
 
-        this.$.next({type: 'stop', name: this.name, source: this});
+        this.$.next({type: 'STOP', name: this.name, source: this})
     }
 
     mute() {
-        this.howler.mute(true, this.id);
+        this.howler.mute(true, this.id)
     }
 
     unmute() {
-        this.howler.mute(false, this.id);
+        this.howler.mute(false, this.id)
     }
 
     set volume(value) {
-        let volume = this.maxVolume * value;
+        let volume = this.maxVolume * value
 
-        this._volume = volume;
-        this.howler.volume(volume, this.id);
+        this._volume = volume
+        this.howler.volume(volume, this.id)
     }
 
     get volume() {
-        return this._volume;
+        return this._volume
     }
 
     fade(from, to, duration) {
-        this.howler.fade(from, to, duration, this.id);
+        this.howler.fade(from, to, duration, this.id)
 
-        this.$.next({type: 'fadeStart', name: this.name, source: this, startVolume: from, endVolume: to});
+        this.$.next({type: 'FADE_START', name: this.name, source: this, startVolume: from, endVolume: to})
     }
 }
 
