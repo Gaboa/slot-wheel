e392875280d9b6bf9461f18b4479fa95f6dda479
Olya <olga.akinshyna@gmail.com>
2018-01-24 12:01:07 +0200

    loadlist updated

--- src/utils/loadlist.js
+++ src/utils/loadlist.js
@@ -32,9 +32,10 @@ const common = [
     { name: 'win_circle', url: 'machine/win_circle.png' },
 
     //transition
-    { name: 'bg', url:'transition/iced.png'},
     { name: 'transition', url:'transition/transition.json'},
-    { name: 'bitmap', url:'transition/ts.json'},
+    { name: 'bitmap', url:'transition/bitmap-export.xml'},
+    { name: 'food', url:'transition/logo.json'},
+    { url: 'preload/preload.json' }
 ]
 
 const mobile = [
