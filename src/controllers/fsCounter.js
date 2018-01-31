2f98c42e73a08dca475c2a6bc7989673d8ff323c
Olya <olga.akinshyna@gmail.com>
2018-01-31 17:57:24 +0200

    fs counter ctrl. listen to multi changes

--- src/controllers/fsCounter.js
+++ src/controllers/fsCounter.js
@@ -1,7 +1,8 @@
 import defaultsDeep from 'lodash.defaultsdeep'
 
 const defaultConfig = {
-    counter: true
+    counter: true,
+    multi: true
 }
 
 export class FSCounterController {
@@ -18,8 +19,8 @@ export class FSCounterController {
         this.data  = game.data
         this.state = game.state
         this.machine = this.level.machine
-        this.counter = this.level.fs.countAmount
-        this.multi   = this.level.fs.multiAmount
+        this.counter = this.level.fs.counter.count
+        this.multi = this.level.fs.counter.multi
 
         if (autoEnable) this.enable()        
     }
@@ -31,7 +32,20 @@ export class FSCounterController {
         this.subs.push(
         this.counterSub = this.data.fs.count.current$
             .sample(this.state.isRolling$)
-            .subscribe(e => this.counter.writeText(e)))
+            .subscribe(e => {
+                this.counter.writeText(e)
+            })
+        )
+
+        if (this.config.multi)
+        this.subs.push(
+        this.counterSub = this.data.fs.multi$
+            .sample(this.state.isRolling$)
+            .distinct()
+            .subscribe(e => {
+                this.multi.writeText(`X${e}`)
+            })
+        )
     }
 
     disable() {
