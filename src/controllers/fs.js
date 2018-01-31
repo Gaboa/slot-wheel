31bbb599198a96763e1dd13d96729f8bce9790cf
Olya <olga.akinshyna@gmail.com>
2018-01-31 10:41:05 +0200

    fs ctrl config updated

--- src/controllers/fs.js
+++ src/controllers/fs.js
@@ -5,18 +5,28 @@ import { zip } from 'rxjs/observable/zip';
 import { defaultNumbersConfig } from '../components/numbers';
 
 const defaultConfig = {
-    multiesMap:{
-        2: 'bonus',
-        3: 'rabbit',
-        4: 'owl',
-        5: 'cat'
+    map: {
+        2: {
+            animal: 'rabbit',
+            item: 'carrot'
+        },
+        3: {
+            animal: 'rat',
+            item: 'cheese'
+        },
+        4: {
+            animal: 'owl',
+            item: 'coffee'
+        },
+        5: {
+            animal: 'cat',
+            item: 'money'
+        }
     },
-    itemsMap:{
-        2: 'cheese',
-        3: 'coffee',
-        4: 'cheese',
-        5: 'coin'
-    }
+    pigs: true,
+    collector: true,
+    afterEachPigWin: true,
+    transition: true
 }
 
 export class FSController{
@@ -39,38 +49,41 @@ export class FSController{
             
         },
         state,
-        config
+        config,
+        autoEnable = true
     }){
         this.game = game
         this.data = data
-        this.state = state
+        this.state = this.game.state
+
         this.config = defaultsDeep(config, defaultConfig)
 
         this.$ = new Subject()
-        this.subs = []
-        this.currentLevel = 0
         
         // delete
         this.pigSimulator()
 
-        this.render()
-        this.enable()
+        if(autoEnable){
+            this.render()
+            this.enable()
+        }
+        
+        
     }
-
+    
     pigSimulator(){
         this.game.root.pig$ = new Subject()
     }
-
+    
     render(){
         this.fsView = new FSView({
             container: this.game.stage,
             game: this.game,
             stream: this.$,
             config:{
-                panel: {},
                 counter:{
                     counter:{},
-                    multi: {},
+                    multi:{},
                     counterBitmap:{
                         general:{
                             text: `${this.data.fs.count.current}`
@@ -84,32 +97,73 @@ export class FSController{
                 },
                 characters:[
                     {
-                        animal:{
-                            general:{
-                                name:`${this.config.multiesMap[this.data.fs.multi]}`
-                            }
+                        general:{
+                            name:`${this.config.map[this.data.fs.multi].animal}`
                         }
                     }
                 ],
                 collector:{
                     settings:{
                         item:{
-                            texture: `${this.config.itemsMap[this.data.fs.multi]}`
+                            texture: `${this.config.map[this.data.fs.multi].item}`
                         }
                     }
                 }
             }
         })
     }
-
+    
     enable(){
+        this.subs = []
+
+        // Start event
+        if (this.config.start)
         this.subs.push(
-            this.pigSub = this.game.root.pig$
-            // some filter on pig events
-            .subscribe( n => {
-                this.$.next(this.data.fs) 
-            }),
-            
+        this.fsStartSub = this.state.mode$
+            .filter(e => e.indexOf('fsBonus'))
+            // TODO: Add start logic
+            .subscribe(e => {
+                // Change view to FS
+                // Add FS Controllers
+                // Start Ticker
+                // Disable Root Controllers (some)
+            }))
+
+        // End event
+        if (this.config.end)
+        this.subs.push(
+        this.fsEndSub = this.state.next$
+            .filter(e => e === 'root')
+            // TODO: Add end logic
+            .subscribe(e => {
+                // Change view to Root ???
+                // Disable FS Controllers
+                // Stop Ticker
+                // Enable Root Controllers (some) ???
+            }))
+
+
+
+
+
+
+        this.fsStartIdleSub = this.state.mode$
+            .filter( e => e)
+            .subscribe(e => {
+                this.game.root.machine.screen.roll()
+                this.state.isIdle = false })
+
+        if(this.config.pigs){
+            this.subs.push(
+                this.pigSub = this.game.root.pig$
+                // some filter on pig events
+                .subscribe( n => {
+                    this.$.next(this.data.fs) 
+                }),
+            )
+        }
+
+        if(this.config.collector){
             this.zippedSub = zip(
                 this.game.root.pig$,
                 this.fsView.$
@@ -118,19 +172,24 @@ export class FSController{
                 if(this.fsView.collector){
                     this.fsView.collector.showItem()
                 }
-            }),
+            })
+        }
 
-            // this.animalSub = this.data.win.lines$
-            //     .sample(this.state.isRolling$)
-            //     .subscribe(n => {
-            //         this.fsView.doItWhenWin()
-            //     })
+        if(this.config.afterEachPigWin){
+            this.subs.push(
+                // this.animalSub = this.data.win.lines$
+                //     .sample(this.state.isRolling$)
+                //     .subscribe(n => {
+                    //         this.fsView.doItWhenWin()
+                    //     })
+                    
+            ) 
+        }
 
-        )    
         this.fsView.$.next({})
     }
-
+        
     disable(){
-        this.subs.forEach( sub => sub.unsubscribe())
+         this.subs.forEach( sub => sub.unsubscribe())
     }
 }
